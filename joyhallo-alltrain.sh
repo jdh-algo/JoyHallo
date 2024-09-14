ee55a1ac77d6261c81485457bc2bbbbf76b38de0
shisheng7 <shisheng7@jd.com>
2024-09-14 18:58:22 +0800

    update homepage

--- joyhallo-alltrain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# stage 1
-accelerate launch -m --config_file accelerate_config.yaml --machine_rank 0 --main_process_ip 0.0.0.0 --main_process_port 20055 --num_machines 1 --num_processes 8 scripts.train_stage1_alltrain --config ./configs/train/stage1_alltrain.yaml && \
-
-# stage 2
-accelerate launch -m --config_file accelerate_config.yaml --machine_rank 0 --main_process_ip 0.0.0.0 --main_process_port 20055 --num_machines 1 --num_processes 8 scripts.train_stage2_alltrain --config ./configs/train/stage2_alltrain.yaml
--- joyhallo-infer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# inference
-CUDA_VISIBLE_DEVICES=0 accelerate launch -m --config_file accelerate_config.yaml --machine_rank 0 --main_process_ip 0.0.0.0 --main_process_port 20055 --num_machines 1 --num_processes 1 scripts.inference
